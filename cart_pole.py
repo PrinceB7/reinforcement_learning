e50f9037e6e23f95c1c8d7a9e565d1907cc9eecf
PrinceB7 <bunyod.ibrokhimov@gmail.com>
2021-11-25 21:02:48 +0900

    Add files via upload

---
+++ cart_pole.py
@@ -0,0 +1,160 @@
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+import random
+import gym
+import numpy as np
+from collections import deque
+from keras.models import Model, load_model
+from keras.layers import Input, Dense
+from keras.optimizers import Adam, RMSprop
+
+
+from gym.envs.classic_control.cartpole import CartPoleEnv
+from typing import Callable, Dict, Optional, Sequence, Text
+import math
+
+def P_Model(input_shape, action_space):
+    X_input = Input(input_shape)
+
+    X = Dense(512, input_shape=input_shape, activation="relu", kernel_initializer='he_uniform')(X_input)
+    X = Dense(256, activation="relu", kernel_initializer='he_uniform')(X)
+    X = Dense(64, activation="relu", kernel_initializer='he_uniform')(X)
+    X = Dense(action_space, activation="linear", kernel_initializer='he_uniform')(X)
+
+    model = Model(inputs = X_input, outputs = X, name='CartPole DQN model')
+    model.compile(loss="mse", optimizer=RMSprop(lr=0.00025, rho=0.95, epsilon=0.01), metrics=["accuracy"])
+
+    model.summary()
+    return model
+
+
+#env_name = 'CartPole-v1'
+#env = gym.make('CartPole-v1')
+
+#'''
+
+class CustomCartPoleEnv(CartPoleEnv):
+    
+    def __init__(self):
+        super().__init__()
+#'''
+
+
+class Bot:
+    def __init__(self):
+        self.env = CustomCartPoleEnv()
+        self.state_size = self.env.observation_space.shape[0]
+        self.env.theta_threshold_radians = 0.385398
+        self.action_size = self.env.action_space.n
+        self.EPISODES = 500
+        self.memory = deque(maxlen=2000)
+        
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0  # exploration rate
+        self.epsilon_min = 0.1
+        self.epsilon_decay = 0.999
+        self.batch_size = 64
+        self.train_start = 1000
+
+        # create main model
+        self.model = P_Model(input_shape=(self.state_size,), action_space = self.action_size)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+        if len(self.memory) > self.train_start:
+            if self.epsilon > self.epsilon_min:
+                self.epsilon *= self.epsilon_decay
+
+    def act(self, state):
+        if np.random.random() <= self.epsilon:
+            return random.randrange(self.action_size)
+        else:
+            return np.argmax(self.model.predict(state))
+
+    def replay(self):
+        if len(self.memory) < self.train_start:
+            return
+        
+        minibatch = random.sample(self.memory, min(len(self.memory), self.batch_size))
+
+        state = np.zeros((self.batch_size, self.state_size))
+        next_state = np.zeros((self.batch_size, self.state_size))
+        action, reward, done = [], [], []
+
+        
+        for i in range(self.batch_size):
+            state[i] = minibatch[i][0]
+            action.append(minibatch[i][1])
+            reward.append(minibatch[i][2])
+            next_state[i] = minibatch[i][3]
+            done.append(minibatch[i][4])
+
+        # do batch prediction to save speed
+        target = self.model.predict(state)
+        target_next = self.model.predict(next_state)
+
+        for i in range(self.batch_size):
+            
+            if done[i]:
+                target[i][action[i]] = reward[i]
+            else:
+                target[i][action[i]] = reward[i] + self.gamma * (np.amax(target_next[i]))
+
+        
+        self.model.fit(state, target, batch_size=self.batch_size, verbose=0)
+
+
+    def load(self, name):
+        self.model = load_model(name)
+
+    def save(self, name):
+        self.model.save(name)
+            
+    def train(self):
+        for e in range(self.EPISODES):
+            state = self.env.reset()
+            state = np.reshape(state, [1, self.state_size])
+            done = False
+            i = 0
+            while not done:
+                self.env.render()
+                action = self.act(state)
+                next_state, reward, done, _ = self.env.step(action)
+                next_state = np.reshape(next_state, [1, self.state_size])
+                if not done:
+                    reward = reward
+                else:
+                    reward = -100
+                self.remember(state, action, reward, next_state, done)
+                state = next_state
+                i += 1
+                if done:                   
+                    print("trial: {}/{}, score: {}, e: {:.2}".format(e, self.EPISODES, i, self.epsilon))
+                    if i >= 500:
+                        print("Saving trained model as cartpole-dqn.h5")
+                        self.save("cartpole-dqn_2.h5")
+                        return
+                self.replay()
+
+    def test(self):
+        self.load("Models/cartpole-dqn.h5")
+        for e in range(self.EPISODES):
+            state = self.env.reset()
+            state = np.reshape(state, [1, self.state_size])
+            done = False
+            i = 0
+            while not done:
+                self.env.render()
+                action = np.argmax(self.model.predict(state))
+                next_state, reward, done, _ = self.env.step(action)
+                state = np.reshape(next_state, [1, self.state_size])
+                i += 1
+                if done:
+                    print("trail: {}/{}, score: {}".format(e, self.EPISODES, i))
+                    break
+
+if __name__ == "__main__":
+    agent = Bot()
+    #agent.train()
+    agent.test()
